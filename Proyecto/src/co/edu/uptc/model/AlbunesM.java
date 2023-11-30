12d6f97fac5aff5fe862f76c0c8ddb10376b7504
Josephgonzalez0110 <harold.gonzalez03@uptc.edu.co>
2023-11-29 21:10:44 -0500

    se agregan las clases de gestion de albunes

--- Proyecto/src/co/edu/uptc/model/AlbunesM.java
+++ Proyecto/src/co/edu/uptc/model/AlbunesM.java
@@ -1,5 +1,51 @@
 package co.edu.uptc.model;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 public class AlbunesM {
+String name;
+String cover;
+String description;
+LocalDate releaseDate;
+String genre;
+String recordLabel;
+
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getCover() {
+	return cover;
+}
+public void setCover(String cover) {
+	this.cover = cover;
+}
+public String getDescription() {
+	return description;
+}
+public void setDescription(String description) {
+	this.description = description;
+}
+public LocalDate getReleaseDate() {
+	return releaseDate;
+}
+public void setReleaseDate(LocalDate releaseDate) {
+	this.releaseDate = releaseDate;
+}
+public String getGenre() {
+	return genre;
+}
+public void setGenre(String genre) {
+	this.genre = genre;
+}
+public String getRecordLabel() {
+	return recordLabel;
+}
+public void setRecordLabel(String recordLabel) {
+	this.recordLabel = recordLabel;
+}
 
 }
