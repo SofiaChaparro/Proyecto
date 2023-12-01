a9221af35b1dd95b91734ab2d9d45901f660d0a1
ASUS <2021Angela>
2023-11-30 19:27:18 -0500

    Se creo la clase ValidarDatos la cual contiene dos metodos que validan que se han solo numeros o solo letras Se creo en la clase coleccionista el metodo de registar y el metodo login

--- Proyecto/src/co/edu/uptc/controller/ColeccionistaC.java
+++ Proyecto/src/co/edu/uptc/controller/ColeccionistaC.java
@@ -1,5 +1,15 @@
 package co.edu.uptc.controller;
 
+import co.edu.uptc.model.ColeccionistaM;
+
 public class ColeccionistaC {
 
+	ColeccionistaM objColeccionistaM=new ColeccionistaM();
+	
+	public String mtdRegistrarC(ColeccionistaM objCol) {
+		return objColeccionistaM.mtdRegistrarM(objCol);
+	}
+	public String mtdLoginC(String e, String c) {
+		return objColeccionistaM.mtdLoginM(e, c);
+	}
 }
---
+++ Proyecto/src/co/edu/uptc/controller/ValidarDatosC.java
@@ -0,0 +1,17 @@
+package co.edu.uptc.controller;
+
+import co.edu.uptc.model.ValidarDatosM;
+
+public class ValidarDatosC {
+
+	ValidarDatosM objValidarDatosM=new ValidarDatosM();
+
+	public boolean validarLetrasC(String cadena) {
+
+		return  objValidarDatosM.validarLetrasM(cadena);
+	}
+	
+	public boolean validarNumerosC(String cadena) {
+		return objValidarDatosM.validarNumerosM(cadena);
+	}
+}
